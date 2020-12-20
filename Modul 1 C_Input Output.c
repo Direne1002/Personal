e46c7559f39bc5cf33fbd1848dee73b25ef71e32
Direne1002 <76035029+Direne1002@users.noreply.github.com>
2020-12-20 12:23:48 +0700

    Update Modul 1 C_Input Output.c

--- Modul 1 C_Input Output.c
+++ Modul 1 C_Input Output.c
@@ -1,3 +1,5 @@
+//This source code is intended to make LED light in ATMEGA8535 blink from the most right lamp to the most left lamp and then back again infinitely 
+
 #include <avr_io.h>
 #define F_CPU 8000000UL //Menggunakan 8MHz clock kristal
 #include <util/delay.h>
