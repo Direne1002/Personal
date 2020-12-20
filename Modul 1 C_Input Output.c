4414fc69d480770fb0f9a6ddb948827e6da79c99
Direne1002 <76035029+Direne1002@users.noreply.github.com>
2020-12-20 12:21:24 +0700

    Add files via upload

---
+++ Modul 1 C_Input Output.c
@@ -0,0 +1,27 @@
+#include <avr_io.h>
+#define F_CPU 8000000UL //Menggunakan 8MHz clock kristal
+#include <util/delay.h>
+int main(void)
+{
+	unsigned char i = 0xFE;
+	unsigned char n;
+	DDRA = 0xFF;
+	PORTA = i;
+	while(1)
+	{
+		for(n=0;n<7;n++)
+		{
+			PORTA = i;
+			_delay_ms(120);
+			i=(i<<1)|(i>>7);
+		}
+		
+		for(n=0;n<7;n++)
+		{
+			PORTA = i;
+			_delay_ms(120);
+			i=(i>>1)|(i<<7);
+		}
+	}
+	return 0;
+}
